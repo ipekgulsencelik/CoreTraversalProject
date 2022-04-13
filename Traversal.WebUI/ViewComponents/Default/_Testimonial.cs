5e769bfcb161fa13c338dc3ebb8d04bf1418176e
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-04-13 20:16:19 +0300

    Testimonial Listesi ve Footer Düzeltemeleri

--- Traversal.WebUI/ViewComponents/Default/_Testimonial.cs
+++ Traversal.WebUI/ViewComponents/Default/_Testimonial.cs
@@ -3,14 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Traversal.BusinessLayer.Concrete;
+using Traversal.DataAccessLayer.EntityFramework;
 
 namespace Traversal.WebUI.ViewComponents.Default
 {
     public class _Testimonial : ViewComponent
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EFTestimonialDAL());
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = testimonialManager.TGetList();
+
+            return View(values);
         }
     }
 }
