9d24fff565af812287c761c03b70655330883abe
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-04-20 13:12:28 +0300

    Layout Düzenlemeleri ve Single Page Sayfası

---
+++ Traversal.WebUI/Controllers/DestinationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Traversal.BusinessLayer.Concrete;
+using Traversal.DataAccessLayer.EntityFramework;
+using Traversal.EntityLayer.Concrete;
+
+namespace Traversal.WebUI.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManger = new DestinationManager(new EFDestinationDAL());
+
+        public IActionResult Index()
+        {
+            var values = destinationManger.TGetList();
+
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination destination)
+        {
+            return View();
+        }
+    }
+}
