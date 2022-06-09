cc269762be9fcb65824cdce5df773df810d8a736
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-06-09 15:47:31 +0300

    Member Sayfasında Verilerin Listelenmesi

---
+++ Traversal.WebUI/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Traversal.BusinessLayer.Concrete;
+using Traversal.DataAccessLayer.EntityFramework;
+
+namespace Traversal.WebUI.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManger = new DestinationManager(new EFDestinationDAL());
+
+        public IActionResult Index()
+        {
+            var values = destinationManger.TGetList();
+
+            return View(values);
+        }
+    }
+}
