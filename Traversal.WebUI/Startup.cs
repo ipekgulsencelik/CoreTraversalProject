0fc4753173de9462ac4152c7e05ef157098ffa32
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-06-09 12:08:54 +0300

    Areas ve Member Layout

--- Traversal.WebUI/Startup.cs
+++ Traversal.WebUI/Startup.cs
@@ -73,6 +73,14 @@ namespace Traversal.WebUI
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
