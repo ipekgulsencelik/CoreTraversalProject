58a50e46cb0d7083a90573328aa87b236a84af0d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-06-10 12:48:42 +0300

    Identity ile Login İşlemi

--- Traversal.WebUI/Controllers/LoginController.cs
+++ Traversal.WebUI/Controllers/LoginController.cs
@@ -14,10 +14,12 @@ namespace Traversal.WebUI.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -60,6 +62,26 @@ namespace Traversal.WebUI.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInWiewModel signInModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(signInModel.Username, signInModel.Password, false, true);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destnation");
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
+
             return View();
         }
     }
---
+++ Traversal.WebUI/Models/UserSignInWiewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traversal.WebUI.Models
+{
+    public class UserSignInWiewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string Password { get; set; }
+    }
+}
