3aabeb018ffba97a9643e14333418aa4bda45cbb
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-31 01:23:29 +0300

    Kullanıcıları Rolleri ile Listeleme

--- Traversal.WebUI/Areas/Admin/Controllers/RoleController.cs
+++ Traversal.WebUI/Areas/Admin/Controllers/RoleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Traversal.EntityLayer.Concrete;
@@ -11,10 +12,12 @@ namespace Traversal.WebUI.Areas.Admin.Controllers
     public class RoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppRole> _userManager;
 
-        public RoleController(RoleManager<AppRole> roleManager)
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppRole> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [Route("Index")]
@@ -84,6 +87,32 @@ namespace Traversal.WebUI.Areas.Admin.Controllers
             await _roleManager.UpdateAsync(value);
 
             return RedirectToAction("Index");
+        }
+
+        [Route("UserList")]
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [Route("AssignRole/{id}")]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleID = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+            }
+         
+            return View(roleAssignViewModels);
         }
     }
 }
