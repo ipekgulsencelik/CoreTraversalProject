edbe82ae3f15e859ce9229310b55eafa0e3b1c4a
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-05 17:07:35 +0300

    Bazı Revizeler

---
+++ Traversal.WebUI/ViewComponents/MemberLayout/_MemberLayoutLanguages.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.WebUI.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutLanguages : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
